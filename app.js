75fb49d766974f9fde53247712f2decb6042e058
Leander Christensen <christensenleander@gmail.com>
2023-04-18 23:30:06 +0200

    Added functionality to upload files via ElevenLabs API

--- app.js
+++ app.js
@@ -1,8 +1,13 @@
 const express = require('express');
-//const fs = require('fs');
+const fs = require('fs');
 const YoutubeMp3Downloader = require('youtube-mp3-downloader');
+const axios = require('axios');
+const FormData = require('form-data');
+const fsExtra = require('fs-extra');
 const app = express();
 const port = 3000;
+require('dotenv').config();
+const apiKey = process.env.ELEVEN_LABS;
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(__dirname + '/public'));
@@ -18,34 +23,65 @@ var YD = new YoutubeMp3Downloader({
 	'outputPath': 'cache', // Where should the downloaded and encoded files be stored?
 	'youtubeVideoQuality': 'highestaudio', // What video quality should be used?
 	'queueParallelism': 2, // How many parallel downloads/encodes should be started?
-	'progressTimeout': 2000 // How long should be the interval of the progress reports
+	'progressTimeout': 500 // How long should be the interval of the progress reports
+});
+
+YD.on('finished', function(error, data) {
+    if (error) {
+        console.log(error);
+    } else {
+        console.log('\nDownload and conversion complete');
+		next(data.file);
+    }
+});
+
+YD.on('error', function(error) {
+	console.log(error);
+});
+
+YD.on('progress', function(progress) {
+	process.stdout.write(`Downloading and converting to mp3 - ${Math.floor(progress.progress.percentage)}%\r`);
 });
 
 async function download(url) {
 	const id = url.split('v=')[1];
-	console.log(id);
 	YD.download(id);
-	YD.on('finished', function(data) {
-		console.log(JSON.stringify(data));
-	});
-
-	YD.on('error', function(error) {
-		console.log(error);
-	});
-
-	YD.on('progress', function(progress) {
-		console.log(JSON.stringify(progress));
-	});
 };
 
-async function next() {
-	console.log('next function');
+async function next(file) {
+	console.log(file);
+	const formData = new FormData();
+	formData.append('name', file);
+	const fileName = file.split("/");
+	formData.append('files', fs.createReadStream(file), {
+		filename: fileName[1],
+		contentType: 'audio/mpeg',
+	});
+	formData.append('description', '');
+	formData.append('labels', '');
+	
+	// make the API request using axios
+	axios.post('https://api.elevenlabs.io/v1/voices/add', formData, {
+	  headers: {
+		'accept': 'application/json',
+		'xi-api-key': apiKey,
+		...formData.getHeaders(),
+	  },
+	})
+	  .then(response => {
+		console.log(response.data);
+		fs.unlink(file);
+	  })
+	  .catch(error => {
+		console.error(error);
+	  });
 }
+
+fsExtra.emptyDirSync('cache');
 
 app.post('/download', async function (req, res) {
 	console.log(`url: ${req.body.url}`);
 	await download(req.body.url);
-	//await next();
 });
 
 app.listen(port, () => {
