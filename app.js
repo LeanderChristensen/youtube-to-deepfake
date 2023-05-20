4368e072a8643fae7c93829c0c5fe5598980c418
Leander Christensen <christensenleander@gmail.com>
2023-05-20 16:43:11 +0200

    Added autodeletion of voices and did final touches

--- app.js
+++ app.js
@@ -4,7 +4,6 @@ const YoutubeMp3Downloader = require('youtube-mp3-downloader');
 const axios = require('axios');
 const FormData = require('form-data');
 const fsExtra = require('fs-extra');
-const socketio = require('socket.io');
 const http = require('http');
 const https = require('https');
 const app = express();
@@ -12,6 +11,8 @@ const port = 3000;
 const server = http.createServer(app);
 require('dotenv').config();
 const apiKey = process.env.ELEVEN_LABS;
+
+const VOICES_MAX = 10; // Max number of voices allowed
 var voices = [];
 let timeouts = {};
 
@@ -19,45 +20,46 @@ app.use(express.urlencoded({ extended: false }));
 app.use(express.static(__dirname + '/public'));
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
-const io = socketio(server);
-const uuid = require('uuid');
 
 app.get('/', (req, res) => {
 	res.render("index");
 });
 
-var YD = new YoutubeMp3Downloader({
-	'ffmpegPath': '/opt/homebrew/Cellar/ffmpeg/6.0/bin/ffmpeg',
-	'outputPath': 'cache',
-	'youtubeVideoQuality': 'highestaudio',
-	'queueParallelism': 5, // How many parallel downloads/encodes should be started?
-	'progressTimeout': 500 // How long should be the interval of the progress reports
-});
-
-YD.on('error', function(error) {
-	console.log(error);
-});
-
-YD.on('progress', function(progress) {
-	const percentage = Math.floor(progress.progress.percentage);
-	process.stdout.write(`Downloading and converting to mp3 - ${percentage}%\r`);
-	io.emit('progress', percentage);
-});
-
 async function download(url) {
-	const id = url.split('v=')[1];
-	return new Promise((resolve, reject) => {
-		YD.on('finished', function (error, data) {
-		  if (error) {
-			console.log(error);
-			reject(error); // reject the Promise if there's an error
-		  } else {
-			console.log('\nDownload and conversion complete');
-			resolve(data.file); // resolve the Promise with the file name
-		  }
+	const YD = new YoutubeMp3Downloader({
+	  ffmpegPath: '/opt/homebrew/Cellar/ffmpeg/6.0/bin/ffmpeg',
+	  outputPath: 'cache',
+	  youtubeVideoQuality: 'highestaudio',
+	  queueParallelism: 5,
+	  progressTimeout: 500
+	});
+  
+	YD.on('error', function (error) {
+	  console.log(error);
+	});
+  
+	try {
+		var id;
+		if (url.search(".be/") == -1) {
+			id = url.split('v=')[1].split('&')[0];
+		} else {
+			id = url.split('.be/')[1].split('?')[0];
+		}
+		return new Promise((resolve, reject) => {
+			YD.on('finished', function (error, data) {
+				if (error) {
+					console.log(error);
+					reject(error);
+				} else {
+					console.log('\nDownload and conversion complete');
+					resolve(data.file);
+				}
+			});
+			YD.download(id);
 		});
-		YD.download(id);
-	  });
+	} catch (error) {
+	  console.error(error);
+	}
 };
 
 async function next(file, retryCount = 0) {
@@ -96,7 +98,7 @@ async function next(file, retryCount = 0) {
       console.log(`Retrying request for ${file} - retry count: ${retryCount}`);
       return next(file, retryCount + 1); // retry the request with the same file
     }
-    throw error; // throw the error to be caught in the calling function
+    throw error;
   }
 }
 
@@ -118,26 +120,28 @@ async function tts(text, voice_id) {
 	  },
 	  responseType: 'arraybuffer'
 	};
-	
 	try {
 	  const response = await axios.post(`https://api.elevenlabs.io/v1/text-to-speech/${voice_id}`, data, config);
 	  const base64response = Buffer.from(response.data).toString('base64');
 	  return base64response;
 	} catch (error) {
 	  console.error(error);
-	  return null; // or return a default value or handle the error in another way
+	  return null;
 	}
 }  
-
-fsExtra.emptyDirSync('cache');
 
 app.post('/download', async function (req, res) {
 	console.log(`url: ${req.body.url}`);
 	const file = await download(req.body.url);
-	const voiceId = await next(file);
-	setUserTimeout(voiceId);
-	voices.push(voiceId);
-	res.json({ voice_id: voiceId });
+	const voiceIds = await getVoices();
+	if (voiceIds.length < VOICES_MAX) {
+		const voiceId = await next(file);
+		setUserTimeout(voiceId);
+		voices.push(voiceId);
+		res.json({ voice_id: voiceId });
+	} else {
+		res.json(0);
+	}
 });
 
 app.post('/tts', async function (req, res) {
@@ -156,7 +160,7 @@ async function deleteVoice(voice) {
 	};
 	try {
 		const response = await axios.delete(`https://api.elevenlabs.io/v1/voices/${voice}`, config);
-		console.log(`Deleted ${voice} - ${response.status}`);
+		console.log(`Deleted ${voice}`);
 	} catch (error) {
 		console.error(`Error deleting voice ${voice}: ${error.message}`);
 	}
@@ -165,18 +169,48 @@ async function deleteVoice(voice) {
 function setUserTimeout(userId) {
 	clearTimeout(timeouts[userId]);
 	timeouts[userId] = setTimeout(() => {
-		// Perform logout action for user with userId here
-		// Remove the timeout ID for this user
-		delete timeouts[userId];
-		deleteVoice(userId);
+		delete timeouts[userId]; // Disconnect user
+		deleteVoice(userId); // Remove the timeout ID for this user
 	}, 10 * 60 * 1000); // 10 minutes
 }
 
-app.get('/checkSessionExpired/:userId', (req, res) => {
+async function getVoices() {
+	const config = {
+		headers: {
+		  'xi-api-key': apiKey
+		}
+	};
+	try {
+		const response = await axios.get(`https://api.elevenlabs.io/v1/voices`, config);
+		const clonedVoices = response.data.voices.filter(voice => voice.category === "cloned");
+		const voiceIds = clonedVoices.map(voice => voice.voice_id);
+		return voiceIds;
+	} catch (error) {
+		console.error(`Error getting voices: ${error.message}`);
+	}
+}
+
+async function cleanUp() {
+	const voiceIds = await getVoices();
+	voiceIds.forEach(voiceId => {
+		deleteVoice(voiceId);
+	});
+	fsExtra.emptyDirSync('cache');
+}
+
+app.get('/checkSessionExpired/:userId', async function (req, res) {
 	const { userId } = req.params;
 	if (timeouts[userId]) { res.send(true) }
 });
 
+app.get('/checkIfFull', async function (req, res) {
+	const voiceIds = await getVoices();
+	if (voiceIds.length == VOICES_MAX) {
+		res.send(true);
+	} else { res.json(1); }
+});
+
 server.listen(3000, () => {
 	console.log('Listening on localhost:3000');
+	cleanUp();
 });
